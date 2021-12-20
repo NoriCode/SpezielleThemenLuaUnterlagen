9560648a4e23d3eb6cb49d1bfb9f831e6cf1a2ad
Philip <norikitamo@web.de>
2021-12-20 15:03:14 +0100

    zusätze nach der Präsentation

--- src/BeispielCodes/Vortragsfolien/functions.lua
+++ src/BeispielCodes/Vortragsfolien/functions.lua
@@ -5,3 +5,9 @@ end
 for i = 0, 20 do
     print("x= " .. i, " y= " .. polynom1(i))
 end
+
+function polynom1(x,x)
+ print("Funktion 2 Para")
+end
+
+polynom1(1,2)
---
+++ src/BeispielCodes/Vortragsfolien/test.lua
@@ -0,0 +1,5 @@
+int a = 0
+while (a < 5) do
+	print("hallo")
+	a = a + 1
+end
